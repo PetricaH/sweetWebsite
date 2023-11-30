71ee41fc2baf2098015862a28a99edd50395ae9d
Petrica Hreniuc <120740177+PetricaH@users.noreply.github.com>
2023-11-30 13:13:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function increaseQuantity() {
+    var quantityInput = document.getElementById('quantity');
+    var currentQuantity = parseInt(quantityInput.value);
+    quantityInput.value = currentQuantity + 1;
+}
+
+function decreaseQuantity() {
+    var quantityInput = document.getElementById('quantity');
+    var currentQuantity = parseInt(quantityInput.value);
+
+    if (currentQuantity > 1) {
+        quantityInput.value = currentQuantity - 1;
+    }
+}
